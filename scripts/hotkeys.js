d281505edbfea9a7a48160a21f27000d97289c2c
metoule <metouleuptheworld@gmail.com>
2012-07-07 23:12:38 +0200

    Added a listener to retrieve LOCSTO in content scripts

--- scripts/hotkeys.js
+++ scripts/hotkeys.js
@@ -3,24 +3,25 @@ window.addEventListener('keydown', keyboardNavigation, false);
 
 function keyboardNavigation(e) 
 {
-	LOCSTO.loadOptions(); // otherwise options might not be up to date
-	
-	if (eventMatchHotKey(e, LOCSTO.prevHotKey))
-	{
-		chrome.extension.sendRequest({ type: "controlPlayer", command: "prev", source: "hotkey" });
-	}
-	else if (eventMatchHotKey(e, LOCSTO.playPauseHotKey))
-	{
-		chrome.extension.sendRequest({ type: "controlPlayer", command: "playpause", source: "hotkey" });
-	} 
-	else if (eventMatchHotKey(e, LOCSTO.nextHotKey))
-	{
-		chrome.extension.sendRequest({ type: "controlPlayer", command: "next", source: "hotkey" });
-	}
-	else if (eventMatchHotKey(e, LOCSTO.whatZatSongHotKey))
-	{
-		chrome.extension.sendRequest({ type: "showNotif", source: "hotkey" });
-	}
+	chrome.extension.sendRequest({ type: "getLOCSTO" }, function(LOCSTO) 
+	{ 
+		if (eventMatchHotKey(e, LOCSTO.prevHotKey))
+		{
+			chrome.extension.sendRequest({ type: "controlPlayer", command: "prev", source: "hotkey" });
+		}
+		else if (eventMatchHotKey(e, LOCSTO.playPauseHotKey))
+		{
+			chrome.extension.sendRequest({ type: "controlPlayer", command: "playpause", source: "hotkey" });
+		} 
+		else if (eventMatchHotKey(e, LOCSTO.nextHotKey))
+		{
+			chrome.extension.sendRequest({ type: "controlPlayer", command: "next", source: "hotkey" });
+		}
+		else if (eventMatchHotKey(e, LOCSTO.whatZatSongHotKey))
+		{
+			chrome.extension.sendRequest({ type: "showNotif", source: "hotkey" });
+		}
+	}); 
 }
 
 function eventMatchHotKey(e, iHotKey)
