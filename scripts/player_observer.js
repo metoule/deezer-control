8a428ab8a570e03bf87ace1e5b9bad93625f6bc8
metoule <metoule@users.noreply.github.com>
2013-11-02 18:01:15 +0100

    Merged both observers into one.

--- scripts/player_observer.js
+++ scripts/player_observer.js
@@ -20,21 +20,29 @@ function updateMyPlayerInfo()
 
 // observe the changes of style atttribute of #player_control_play, to track play / pause changes
 // (its style changes from hidden to display)
+// observe the changes of content of #player_track_title, to track song changes
 var observerPlay = new MutationObserver(function(mutations) 
 {
-	if (mutations.length == 1)
-    {
-		var mutation = mutations[0];
-		if (mutation.oldValue != mutation.target.getAttribute(mutation.attributeName))
-			updateMyPlayerInfo();
-    }
-});
-observerPlay.observe(document.getElementById('player_control_play'), { attributes: true, attributeOldValue: true, attributeFilter: ['style'] });  
+	var bUpdateInfo = false;
+	for (var i = 0; i < mutations.length && !bUpdateInfo; i++)
+	{
+		var mutation = mutations[i];
+		
+		// result of 'player_control_play' observer
+		if (mutation.type == "attributes")
+			bUpdateInfo  = mutation.oldValue != mutation.target.getAttribute(mutation.attributeName);
+		
+		// result of 'player_track_title' observer
+		else if (mutation.type == "characterData" || mutation.type == "childList")
+			bUpdateInfo = true;
+	}
 
+	if (bUpdateInfo)
+		updateMyPlayerInfo();
+});  
+observerPlay.observe(document.getElementById('player_track_title'),  { childList: true, characterData: true });  
+observerPlay.observe(document.getElementById("player_control_play"), { attributes: true, attributeOldValue: true, attributeFilter: ['style'] });
 
-// observe the changes of content of #player_track_title, to track song changes
-var observerTrackTitle = new MutationObserver(function(mutations) { updateMyPlayerInfo(); });
-observerTrackTitle.observe(document.getElementById('player_track_title'), { childList: true, characterData: true }); 
 
 // update content on first load
 updateMyPlayerInfo();
