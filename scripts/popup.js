e34244c1e5d012addb2595c53687eba81ffcc524
metoule <metoule@users.noreply.github.com>
2014-03-29 13:46:52 +0100

    Migration from vanilla JS to jQuery Renamed div for deezer data from myPlayerInfo to DeezerControlData

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,23 @@
 
-window.addEventListener('load', function(e) { preparePopup(); });
+window.addEventListener('load', function(e) 
+{
+	jQueryExtension();
+	preparePopup(); 
+});
 
+// get url parameters
 function gup(iParamName)
 {
 	// taken from http://www.netlobo.com/url_query_string_javascript.html
-	var aParamName = iParamName.replace(/[\[]/,"\\[").replace(/[\]]/,"\\]");  
-	var regexS = "[\\?&]" + aParamName + "=([^&#]*)";  
-	var regex = new RegExp(regexS);  
-	var results = regex.exec(window.location.href); 
-	if (results == null)
-		return null;  
-	else
-		return results[1];
+	var aParamName = iParamName.replace(/[\[]/,"\\[").replace(/[\]]/,"\\]"),  
+		regexS = "[\\?&]" + aParamName + "=([^&#]*)",
+		regex = new RegExp(regexS),
+		results = regex.exec(window.location.href);
+	
+	if (results === null)
+		return null;
+
+	return results[1];
 }
 
 function preparePopup()
@@ -26,19 +32,12 @@ function preparePopup()
 	$('#now_playing_info_track').click(function ()  { executeDoAction('linkCurrentSong');   return false; });
 	$('#now_playing_info_artist').click(function () { executeDoAction('linkCurrentArtist'); return false; });
 	
-	// add tooltip in case of ellipsis
-	$('#now_playing_info > span').on('mouseenter', function () 
-	{
-		if (this.offsetWidth < this.scrollWidth && !$(this).attr('title'))
-			$(this).attr('title', $(this).text()); 
-	});
-	
 	// reset popup
-	setTimeout(refreshPopup, 0);
+	refreshPopup();
 	
 	// we're in a notif scenario, add mouse over and mouse out
-	if (gup('notif') == 'on')
-	{		
+	if (gup('notif') === 'on')
+	{
 		window.addEventListener('mouseover', function(e) 
 				{ 
 					chrome.extension.getBackgroundPage().NOTIFS.onMouseOverNotif();
@@ -64,8 +63,8 @@ function loadStyle(iPopupStyle)
 	if (aPopupStyle)
 	{
 		// set the style based on the preference
-		if (document.getElementById("popup_style_css") != null)
-			document.getElementById("popup_style_css").href = "css/" + aPopupStyle + "/popup.css";
+		if ($("#popup_style_css").length)
+			$("#popup_style_css").attr('href', 'css/' + aPopupStyle + '/popup.css');
 
 		// on resize, we want to fetch the new cover to avoid pixelisation 
 		// on size change from small to large
@@ -75,7 +74,7 @@ function loadStyle(iPopupStyle)
 		COVER_SIZE = COVER_SIZES[aPopupStyle];
 		
 		// replace img src to show new size
-		document.getElementById('cover').src = document.getElementById('cover').src.replace(aOldSize, COVER_SIZE);
+		$('#cover').attr('src', function(index, oldSrc) { return oldSrc.replace(aOldSize, COVER_SIZE); });
 	}
 }
 
@@ -96,32 +95,47 @@ function refreshPopup()
 	// get now playing info from background page
 	var aNowPlayingData = chrome.extension.getBackgroundPage().gNowPlayingData;
 	if (aNowPlayingData != null)
-	{
-		document.getElementById('now_playing_info').style.visibility = "visible";
-		
+	{	
 		// show pause or play button 
-		if (aNowPlayingData.dz_playing == 'true')
-		{
-			document.getElementById('control-pause').style.display = "inline-block";
-			document.getElementById('control-play').style.display  = "none";
-		} else {
-			document.getElementById('control-pause').style.display = "none";
-			document.getElementById('control-play').style.display  = "inline-block";
-		}
+		var showPause = aNowPlayingData.dz_playing === 'true';
+		$('#control-play').css('display', 'inline-block').toggle(!showPause);
+		$('#control-pause').css('display', 'inline-block').toggle(showPause);
 		
 		// set track title and artist
+		$('#now_playing_info').show();
 		$('#now_playing_info_track').text(aNowPlayingData.dz_track);
 		$('#now_playing_info_artist').text(aNowPlayingData.dz_artist);
 		
-		// show or hide prev / next buttons if needed
-		document.getElementById('control-prev').style.visibility = (aNowPlayingData.dz_is_prev_active == 'true' ? "visible" : "hidden");
-		document.getElementById('control-next').style.visibility = (aNowPlayingData.dz_is_next_active == 'true' ? "visible" : "hidden");
+		// add tooltip in case of ellipsis
+		$('#now_playing_info > span').each(function (index) 
+		{
+			var title = '';
+			if (this.offsetWidth < this.scrollWidth)
+				title = $(this).text();
+			$(this).attr('title', title);
+		});
 		
+		// show or hide prev / next buttons if needed
+		$('#control-prev').visibilityToggle(aNowPlayingData.dz_is_prev_active == 'true');
+		$('#control-next').visibilityToggle(aNowPlayingData.dz_is_next_active == 'true');
+
 		// get the cover (can be ANY SIZE WE WANT :))
-		document.getElementById('cover').src = "http://cdn-images.deezer.com/images/cover/" + aNowPlayingData.dz_cover + "/" + COVER_SIZE + "-000000-80-0-0.jpg";
-	} else {
-		document.getElementById('control-pause').style.display = "none";
-		document.getElementById('now_playing_info').style.visibility = "hidden";
-		document.getElementById('cover').src = "imgs/unknown_cd.png";
+		$('#cover').attr('src', "http://cdn-images.deezer.com/images/cover/" + aNowPlayingData.dz_cover + "/" + COVER_SIZE + "-000000-80-0-0.jpg");
+	} 
+	else 
+	{
+		$('#now_playing_info').hide();
+		$('#control-pause').hide();
+		$('#cover').attr('src', "imgs/unknown_cd.png");
 	}
+}
+
+function jQueryExtension()
+{
+	$.fn.extend({
+		visibilityToggle: function(showOrHide)
+		{
+		    return this.css('visibility', showOrHide === true ? 'visible' : 'hidden');
+		}
+	});
 }
