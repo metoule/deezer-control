6ccfeb4f8e6edb68f49057b86df537464f35aec9
metoule <metoule@localhost>
2014-10-09 17:59:38 +0200

    Added support of RadioFG.com

---
+++ scripts/radiofg/player_observer.js
@@ -0,0 +1,113 @@
+
+// get data from JS object dzPlayer
+function updateDeezerControlData()
+{
+	"use strict";
+	var DeezerControlData = document.getElementById('DeezerControlData');
+	
+	DeezerControlData.setAttribute('dz_is_active',   true);
+	DeezerControlData.setAttribute('dz_playing',	 $('#playBtn').css('display') === 'none');
+	DeezerControlData.setAttribute('dz_artist',	     $("#artistSong").text().trim());
+	DeezerControlData.setAttribute('dz_track',	     $("#titleSong").text().trim());
+	DeezerControlData.setAttribute('dz_artist_id',   "");
+	DeezerControlData.setAttribute('dz_album_id',    "");
+	DeezerControlData.setAttribute('dz_cover',	     $('#coverimg').attr("src"));
+	DeezerControlData.setAttribute('dz_prev_cover',  "");
+	DeezerControlData.setAttribute('dz_next_cover',  "");
+	DeezerControlData.setAttribute('dz_is_prev_active', false);
+	DeezerControlData.setAttribute('dz_is_next_active', false);
+	document.getElementById('lastUpdate').textContent = Math.floor(new Date().getTime()); 
+}
+
+function deezerControlMethod_pause()
+{
+	"use strict";
+	$("#stopBtn").click();
+}
+
+function deezerControlMethod_play()
+{
+	"use strict";
+	$("#playBtn").click();
+}
+
+function deezerControlMethod_prev()
+{
+	"use strict";
+	// NOP
+}
+
+function deezerControlMethod_next()
+{
+	"use strict";
+	// NOP
+}
+
+function deezerControlMethod_linkCurrentSong()
+{
+	"use strict";
+	// NOP
+}
+
+function deezerControlMethod_linkCurrentArtist()
+{
+	"use strict";
+	// NOP
+}
+
+//trigger the observer on removeMe
+function triggerRemoveDeezerData()
+{
+	document.getElementById('removeMe').textContent = "now"; 
+}
+
+// update content on first load
+(function()
+{
+	"use strict";
+	
+	var player_track_title = $("#artistSong, #titleSong");
+	var player_control_play = $("#playBtn");
+	
+	// ensure the player is on the page
+	if (player_track_title.length > 0 && player_control_play.length > 0)
+	{
+		// observe the changes of style atttribute of #player_control_play, to track play / pause changes
+		// (its style changes from hidden to display)
+		// observe the changes of content of #player_track_title, to track song changes
+		var observerPlay = new MutationObserver(function(mutations) 
+		{
+			"use strict";
+			
+			var bUpdateInfo = false, i, mutation;
+			for (i = 0; i < mutations.length && !bUpdateInfo; i++)
+			{
+				mutation = mutations[i];
+				
+				// result of 'player_control_play' observer
+				if (mutation.type === "attributes")
+				{
+					bUpdateInfo  = mutation.oldValue !== mutation.target.getAttribute(mutation.attributeName);
+				}
+				// result of 'player_track_title' observer
+				else if (mutation.type === "characterData" || mutation.type === "childList")
+				{
+					bUpdateInfo = true;
+				}
+			}
+
+			if (bUpdateInfo)
+			{
+				updateDeezerControlData();
+			}
+		});
+		
+		player_track_title.each(function ()  { observerPlay.observe(this, { childList: true, characterData: true }); });
+		player_control_play.each(function () { observerPlay.observe(this, { attributes: true, attributeOldValue: true, attributeFilter: ['style', 'data-action'] }); });
+
+		updateDeezerControlData();
+	}
+	// failure to initialize
+	else 
+		triggerRemoveDeezerData();
+})();
