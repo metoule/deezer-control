56f554775c4ea67d877bc66c55d8a8e9a7c335f0
metoule <metoule@users.noreply.github.com>
2014-10-12 23:10:21 +0200

    Fix new Deezer support: monitor sidebar to ensure we only load the observer script when the wanted elements are present

--- scripts/deezer/bootstrap.js
+++ scripts/deezer/bootstrap.js
@@ -1,9 +1,18 @@
 
-function bootstrap()
+var gCheckIfReady = new MutationObserver(function(mutations) 
 {
-	if (document.readyState !== "complete")
-		return;
+	"use strict";
 	
+	if (!okForLoad())
+		return;
+
+	loadObserver();
+	this.disconnect(); // stop observing
+});
+
+
+function loadObserver()
+{
 	// inject a new JS script that can interact with the JS objects of the page
 	var s = document.createElement('script');
 	s.src = chrome.extension.getURL('scripts/deezer/player_observer.js');
@@ -12,6 +21,36 @@ function bootstrap()
 	
 	// set player name
 	document.getElementById('DeezerControlData').setAttribute('dz_name', 'deezer');
+}
+
+// do we have the elements needed to work?
+function okForLoad()
+{
+	return document.querySelector("#player_track_title, .player-track-title span") !== null;
+}
+
+function bootstrap()
+{
+	if (document.readyState !== "complete")
+		return;
+	
+	// everything might already be loaded
+	if (okForLoad())
+	{
+		loadObserver();
+		return;
+	}	
+
+	// delay insertion until the elements we want are added
+	var sidebar = document.getElementById("page_sidebar");
+	if (sidebar === null)
+	{
+		// can't find the sidebar to monitor load - abort
+		document.getElementById('removeMe').textContent = "now";
+		return;
+	}
+		
+	gCheckIfReady.observe(sidebar, { subtree: true, childList: true });
 }
 
 document.addEventListener("readystatechange", bootstrap);
