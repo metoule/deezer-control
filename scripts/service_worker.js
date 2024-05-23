18f5d741ab4521a75db03c4ef2b689d74841ff0e
metoule <metoule@users.noreply.github.com>
2024-05-23 21:44:36 +0200

    Migrate manifest

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,10 +1,9 @@
 import Version from './Version.js';
 
-const version = new Version("2.0.15");
+const version = new Version('2.0.15');
 console.log('service worker loaded', version);
 
 // if no popup is set, it means that we should open a new tab with default player
-chrome.action.onClicked.addListener(function() 
-{
-	chrome.tabs.create({ url: '/options.html' });
+chrome.action.onClicked.addListener(function () {
+  chrome.tabs.create({ url: '/options.html' });
 });
