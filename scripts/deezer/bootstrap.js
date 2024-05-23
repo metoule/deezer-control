04060838f3d396abc74612d1922b093c4597c5ab
metoule <metoule@users.noreply.github.com>
2024-05-23 23:28:04 +0200

    Modernize

--- scripts/deezer/bootstrap.js
+++ scripts/deezer/bootstrap.js
@@ -1,10 +1,9 @@
 function loadObserver() {
   // inject a new JS script that can interact with the JS objects of the page
-  var s = document.createElement('script');
+  const s = document.createElement('script');
   s.src = chrome.extension.getURL('scripts/deezer/player_observer.js');
   (document.head || document.documentElement).appendChild(s);
   s.onload = function () {
-    'use strict';
     s.parentNode.removeChild(s);
   };
 
