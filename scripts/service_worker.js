f09b7a3ed647be749add0430efe18427af94f3a0
metoule <metoule@users.noreply.github.com>
2024-06-01 11:16:02 +0200

    Handle popup

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -2,3 +2,9 @@ import Version from './Version.js';
 
 const version = new Version("2.0.15");
 console.log('service worker loaded', version);
+
+// if no popup is set, it means that we should open a new tab with default player
+chrome.action.onClicked.addListener(function() 
+{
+	chrome.tabs.create({ url: '/options.html' });
+});
