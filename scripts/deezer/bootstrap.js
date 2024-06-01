64f55021007e482dc8da88f3d13a4e9aec2434ec
metoule <metoule@users.noreply.github.com>
2024-06-01 13:48:05 +0200

    Revert bootstrap

--- scripts/deezer/bootstrap.js
+++ scripts/deezer/bootstrap.js
@@ -1,9 +1,10 @@
 function loadObserver() {
   // inject a new JS script that can interact with the JS objects of the page
-  const s = document.createElement('script');
+  var s = document.createElement('script');
   s.src = chrome.extension.getURL('scripts/deezer/player_observer.js');
   (document.head || document.documentElement).appendChild(s);
   s.onload = function () {
+    'use strict';
     s.parentNode.removeChild(s);
   };
 
